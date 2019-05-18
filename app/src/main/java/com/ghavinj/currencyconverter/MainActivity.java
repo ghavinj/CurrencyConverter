640a2d10def07a46b5ba056fc378e537e983dc69
GhavinJ <ghavinj@icloud.com>
2019-05-17 23:15:54 -0400

    created method for converting currency.

--- app/src/main/java/com/ghavinj/currencyconverter/MainActivity.java
+++ app/src/main/java/com/ghavinj/currencyconverter/MainActivity.java
@@ -2,10 +2,25 @@ package com.ghavinj.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void convertToBitCoin(View view){
+        EditText amountEditText = (EditText) findViewById(R.id.amountEditText);
 
+        int dollarAmount = Integer.parseInt(amountEditText.getText().toString());
+        float bitCoin = 7301;
+
+        float result = dollarAmount /bitCoin;
+
+        Toast.makeText(this, "You have "+ result + " bitcoin", Toast.LENGTH_LONG).show();
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
